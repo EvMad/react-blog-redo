948e9b62b5968bf7f6487ba71060779996ff28e5
GitPit <rifflifter@gmail.com>
2022-01-06 22:10:51 -0500

    created seed.js in Seeders with const and comments

---
+++ server/Seeders/seed.js
@@ -0,0 +1,5 @@
+const db = require('../config/connection');
+
+// const { model } = require('../models');
+
+//db.once('open', async () => { await model.deleteMany({}); await model.create(modelSeeds); })
