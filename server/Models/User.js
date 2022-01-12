da68746bf1730a21fec5064be539a9ff988b6cef
GitPit <rifflifter@gmail.com>
2022-01-12 17:08:17 -0500

    username = {} password = {}

--- server/Models/User.js
+++ server/Models/User.js
@@ -2,4 +2,16 @@ const { Schema, model } = require('mongoose');
 
 // const bcrypt = require('bcrypt');
 
-const userSchema = new Schema ({});
+const userSchema = new Schema ({
+
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      password: {
+        type: String,
+        required: true,
+        minlength: 8,
+      },
+});
