ca5e0764c7ac4818a1640208b23ce652ae410f87
GitPit <rifflifter@gmail.com>
2022-01-11 23:26:57 -0500

    posts: async

--- server/Schemas/resolvers.js
+++ server/Schemas/resolvers.js
@@ -1,6 +1,6 @@
 const { AuthenticationError } = require("apollo-server-expres");
 
-// const { User, Post } = require('../models');
+const { User, Post } = require('../models');
 
 // const { signToken } = require("../utils/auth");
 
@@ -18,6 +18,13 @@ const resolvers = {
             return User.findOne({ username: username });
         },
 
-        
+        posts: async (parent, { userID }) => {
+
+            if (!userID) {
+                return Post.find().sort({ createdAt: -1 });
+            } else {
+                return Post.find({ user_id: userID }).sort({ createdAt: -1 });
+            }
+        },
     }
 }
