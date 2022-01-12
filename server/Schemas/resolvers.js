78cdbc80cb5096e2545e44ffbbd54d5a5f73401c
GitPit <rifflifter@gmail.com>
2022-01-11 23:20:21 -0500

    const { signToken }

--- server/Schemas/resolvers.js
+++ server/Schemas/resolvers.js
@@ -2,4 +2,6 @@ const { AuthenticationError } = require("apollo-server-expres");
 
 // const { User, Post } = require('../models');
 
+// const { signToken } = require("../utils/auth");
+
 // const resolvers = { Query: {} Mutation: {} }
