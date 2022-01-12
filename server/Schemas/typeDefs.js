096ef039f421ec640d3a1d95ae768a126fc5e35f
GitPit <rifflifter@gmail.com>
2022-01-11 23:28:44 -0500

    type User {}

--- server/Schemas/typeDefs.js
+++ server/Schemas/typeDefs.js
@@ -1,3 +1,12 @@
 const { gql } = require('apollo-server-express');
 
-const typeDefs = gql ``
+const typeDefs = gql `
+type User {
+    _id: ID
+    username: String
+    password: String
+    posts: [Post]!
+}
+
+
+`
