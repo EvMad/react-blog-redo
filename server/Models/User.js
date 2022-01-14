5a847320c5294aa1d5147eeadce61ebf42a97637
GitPit <rifflifter@gmail.com>
2022-01-13 22:21:01 -0500

    added middleware for User.js

--- server/Models/User.js
+++ server/Models/User.js
@@ -1,6 +1,6 @@
 const { Schema, model } = require('mongoose');
 
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema ({
 
@@ -19,9 +19,23 @@ const userSchema = new Schema ({
 // pre-save middleware to create password
 // userSchema.pre('save', async function (next) {})
 
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+
 
 //compare password to hashed password
 // userSchema.methods.isCorrectPassword = async function (password) {}
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 
 const User = model('User', userSchema);
