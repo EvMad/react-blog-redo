ca640755c755646737b29a756253ff13d7722c3e
GitPit <rifflifter@gmail.com>
2022-01-13 22:22:33 -0500

    corrected camelCase for const postSchema

--- server/Models/Post.js
+++ server/Models/Post.js
@@ -2,7 +2,7 @@ const { Schema, model } = require('mongoose');
 
 // const dateFormat = require('../utils/dateFormat');
 
-const PostSchema = new Schema ({
+const postSchema = new Schema ({
 
     postTitle: {
 
