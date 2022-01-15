28da2d4f3c00fd4f2bcf23ac2348aec91ce37c78
GitPit <rifflifter@gmail.com>
2022-01-14 20:03:09 -0500

    db.once('open', async () =>

--- server/Seeders/seed.js
+++ server/Seeders/seed.js
@@ -3,6 +3,23 @@ const { User, Post } = require('../models');
 // const userSeed = require('./userSeed.json');
 // const postSeed = require('./postSeed.json');
 
+db.once('open', async () => {
+    try {
+        await Post.deleteMany({});
+        await User.deleteMany({});
+
+        // await User.create(userSeed);
+
+        // for loop for Post.create(postSeed[i])???
+
+    } catch (err) {
+    console.error(err);
+    process.exit(1);
+}
+
+console.log("db.once('open') run complete");
+process.exit(0);
+});
 
 
-//db.once('open', async () => { await model.deleteMany({}); await model.create(modelSeeds); })
+
