30a03d82d4e7562f057a1fefde69f6f7a3524f29
GitPit <rifflifter@gmail.com>
2022-01-15 17:53:52 -0500

    wrote comments for mutation.js to-do list

--- client/src/Utils/mutation.js
+++ client/src/Utils/mutation.js
@@ -1,1 +1,15 @@
 import { gql } from '@apollo/client';
+
+// export const ADD_USER = gql``
+
+//export const LOGIN = gql``
+
+//export const ADD_POST = gql``
+
+//export const ADD_COMMENT = gql``
+
+//export const EDIT_POST = gql``
+
+//export const REMOVE_POST = gql``
+
+// remove comment? edit comment?
