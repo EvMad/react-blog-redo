6544e36a7f445c22763472805516c714c6d27e33
GitPit <rifflifter@gmail.com>
2022-01-15 17:44:48 -0500

    wrote isTokenExpired()

--- client/src/Utils/auth.js
+++ client/src/Utils/auth.js
@@ -11,5 +11,16 @@ class AuthService {
         return !!token && !this.isTokenExpired(token);
     }
 
+    isTokenExpired(token) {
+        try {
+            const decoded = decode(token);
+            if (decoded.exp < Date.now() / 1000) {
+                return true;
+            } else return false;
+        } catch (err) {
+            return false;
+        }
+    }
+
     
 }
