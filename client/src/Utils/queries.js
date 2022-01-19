8a7ac981dce0d5554f57682009f25288d1eeb38a
GitPit <rifflifter@gmail.com>
2022-01-18 19:25:25 -0500

    wrote export const QUERY_POSTS

--- client/src/Utils/queries.js
+++ client/src/Utils/queries.js
@@ -1,1 +1,17 @@
 import { gql } from '@apollo/client';
+
+export const QUERY_POSTS = gql`
+    query allPosts {
+        posts {
+            _id
+            postContent
+            postAuthor
+            postTitle
+            comments {
+                commentText
+            }
+            createdAt
+        }
+    }
+`;
+
