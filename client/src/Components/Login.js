c34bc432e1a940e1f2de25a6dd2d3863665a9b2c
GitPit <rifflifter@gmail.com>
2022-01-18 21:07:05 -0500

    wrote function Login(props)

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -5,3 +5,28 @@ import { LOGIN } from '../Utils/mutations';
 import Auth from '../Utils/auth';
 
 //function Login(props)
+
+function Login(props) {
+
+    const [formState, setFormState] = useState({ username: '', password: '' });
+    const [login, { error }] = useMutation(LOGIN);
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const mutationResponse = await login({
+                variables: { username: formState.username, password: formState.password },
+            });
+            const token = mutationResponse.data.login.token;
+            console.log(mutationResponse);
+            Auth.login(token);
+        } catch (e) {
+            console.log(e)
+        }
+    };
+
+    // const handleChange
+    
+};
+
