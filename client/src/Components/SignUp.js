a1abc5f5db3e8401c0457e7115fb8800a70860e6
GitPit <rifflifter@gmail.com>
2022-01-19 17:35:58 -0500

    function SignUp(props)

--- client/src/Components/SignUp.js
+++ client/src/Components/SignUp.js
@@ -3,3 +3,12 @@ import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import Auth from '../Utils/auth';
 import { ADD_USER } from '../Utils/mutations';
+
+function SignUp(props) {
+    
+    const [formState, setFormState] = useState({ username: '', password: '' });
+    const [addUser] = useMutation(ADD_USER);
+
+    // const handleFormSubmit
+
+}
