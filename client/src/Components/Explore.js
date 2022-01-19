bdac1bbff0798f05525b808cfb183ed195f7776a
GitPit <rifflifter@gmail.com>
2022-01-19 17:48:03 -0500

    import auth from "../Utils/auth";

--- client/src/Components/Explore.js
+++ client/src/Components/Explore.js
@@ -1,3 +1,6 @@
 import { Link } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { QUERY_POSTS } from "../Utils/queries";
+import auth from "../Utils/auth";
+
+//import images fonts and icons here?
