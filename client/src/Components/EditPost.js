c0f0225fe7fa5961d3dfa9515f73a67203c6048a
GitPit <rifflifter@gmail.com>
2022-01-21 18:14:19 -0500

    import { EDIT_POST } from "../Utils/mutations";

--- client/src/Components/EditPost.js
+++ client/src/Components/EditPost.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Redirect, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
+// import useMutation ?
+import { EDIT_POST } from "../Utils/mutations";
 
 const EditPost = () => {
 
