1b71fdaf602c3621cf1e4181e7889e27615ce9e4
GitPit <rifflifter@gmail.com>
2022-01-21 17:48:35 -0500

    const { postId } const { loading, data }

--- client/src/Components/EditPost.js
+++ client/src/Components/EditPost.js
@@ -1,6 +1,17 @@
 import React from 'react';
 
 const EditPost = () => {
+
+    const { postId } = useParams();
+
+    const { loading, data } = useQuery({ variables: { postId: postId }});
+
+    const singlePost = data?.singlePost || {};
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <div></div>
     );
