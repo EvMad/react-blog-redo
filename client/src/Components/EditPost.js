0e42e5e6125f90f37b1d503b68b3df7a3e430a78
GitPit <rifflifter@gmail.com>
2022-01-21 17:49:23 -0500

    import { Link } from 'react-router-dom';

--- client/src/Components/EditPost.js
+++ client/src/Components/EditPost.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Redirect, useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
 
 const EditPost = () => {
 
