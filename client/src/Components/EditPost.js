2553a08e40d8d47b1101a942ca4c99f7f001ca20
GitPit <rifflifter@gmail.com>
2022-01-21 17:58:15 -0500

    const mutationResponse = await editPost

--- client/src/Components/EditPost.js
+++ client/src/Components/EditPost.js
@@ -21,6 +21,20 @@ const EditPost = () => {
 
     const [editPost] = useMutation(EDIT_POST);
 
+    //copy handleFormSubmit from CreatePost.js?
+
+    const mutationResponse = await editPost ({
+        variables: {
+            postTitle: document.querySelector('#postTitle').value,
+                postContent: document.querySelector('#postContent').value,
+                tag: document.querySelector('#category').value,
+        },
+    });
+    alert('Successfully Edited Post');
+        window.location.assign('/');
+
+    
+
     return (
         <div></div>
     );
