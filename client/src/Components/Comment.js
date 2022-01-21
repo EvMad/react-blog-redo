06c3e417f9f69a857b50139e53d0efc8f85fc414
GitPit <rifflifter@gmail.com>
2022-01-21 18:10:13 -0500

    const mutationResponse = await addComment

--- client/src/Components/Comment.js
+++ client/src/Components/Comment.js
@@ -7,6 +7,16 @@ const Comment = (props) => {
 
     const [addComment] = useMutation(ADD_COMMENT);
 
+    //const handleFormSubmit - needs #ID's for querySelector
+
+    const mutationResponse = await addComment ({
+        variables: {
+            //postId ??
+            commentText: document.querySelector('#comment').value,
+            // commentAuthor: 
+        },
+    });
+
     return (
         <div></div>
     );
