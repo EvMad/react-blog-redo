f57d95de53c49d7cb5e023991ee69f6f0e2305bc
GitPit <rifflifter@gmail.com>
2022-01-21 17:31:20 -0500

    const handleFormSubmit = async (event) =>

--- client/src/Components/CreatePost.js
+++ client/src/Components/CreatePost.js
@@ -7,5 +7,26 @@ import auth from "../Utils/auth";
 const CreatePost = (props) => {
 
     const [addPost] = useMutation(ADD_POST);
-    
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        console.log(document.querySelector('#postTitle').value);
+        // console.log entire post??
+        console.log(document.querySelector('#postContent').value);
+        console.log(document.querySelector('#category').value);
+
+        const mutationResponse = await addPost ({
+            variables: {
+                postTitle: document.querySelector('#postTitle').value,
+                postContent: document.querySelector('#postContent').value,
+                tag: document.querySelector('#category').value,
+            },
+        });
+        alert('Successfully Created Post');
+        window.location.assign('/');
+        };
+
+        // return
+
 }
