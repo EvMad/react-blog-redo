dc95ed806db45d68f6cace72022308e14983e343
GitPit <rifflifter@gmail.com>
2022-01-21 17:23:18 -0500

    created CreatePost.js

---
+++ client/src/Components/CreatePost.js
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+import { useMutation } from "apollo/client";
+import { ADD_POST } from "../Utils/mutations";
+// import css
+import auth from "../Utils/auth";
