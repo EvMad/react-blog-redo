7aebced1f773a2ac92fc529345e64a97afcf4d9c
GitPit <rifflifter@gmail.com>
2022-01-21 17:24:41 -0500

    const CreatePost

--- client/src/Components/CreatePost.js
+++ client/src/Components/CreatePost.js
@@ -3,3 +3,9 @@ import { useMutation } from "apollo/client";
 import { ADD_POST } from "../Utils/mutations";
 // import css
 import auth from "../Utils/auth";
+
+const CreatePost = (props) => {
+
+    const [addPost] = useMutation(ADD_POST);
+    
+}
