faadc668ebe1318741a7dcd20b9712144dd084ff
GitPit <rifflifter@gmail.com>
2022-01-21 17:41:31 -0500

    const ViewPost = () =>

--- client/src/Components/ViewPost.js
+++ client/src/Components/ViewPost.js
@@ -2,3 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Redirect, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
+
+
+const ViewPost = () => {
+
+    const { postId } = useParams();
+
+    const { loading, data } = useQuery({ variables: { postId: postId }});
+
+    const singlePost = data?.singlePost || {};
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+}
