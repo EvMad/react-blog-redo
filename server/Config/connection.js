9b560560e3fa10f88a14207419cf83f4253ca8fd
GitPit <rifflifter@gmail.com>
2022-01-28 18:24:29 -0500

    changed database name path to match repo name

--- server/Config/connection.js
+++ server/Config/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 //requires new database name for localhost/  where am i determining mongo database name?
 
 // mongoose.connect(
-//   process.env.MONGODB_URI || 'mongodb://localhost/the-daily-rebound',
+//   process.env.MONGODB_URI || 'mongodb://localhost/react-blog-redo',
 //   {
 //     useNewUrlParser: true,
 //     useUnifiedTopology: true,
