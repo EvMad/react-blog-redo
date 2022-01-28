950bff5f78ff9e9b5a69b174cfd5c8908bedd640
GitPit <rifflifter@gmail.com>
2022-01-28 18:25:05 -0500

    uncommented mongoose.connect to activate for debug

--- server/Config/connection.js
+++ server/Config/connection.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 
 //requires new database name for localhost/  where am i determining mongo database name?
 
-// mongoose.connect(
-//   process.env.MONGODB_URI || 'mongodb://localhost/react-blog-redo',
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false,
-//   }
-// );
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/react-blog-redo',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 module.exports = mongoose.connection;
