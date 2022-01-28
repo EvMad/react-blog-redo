66e12e310a3d18ee6b593bf98ba204da6ea5d445
GitPit <rifflifter@gmail.com>
2022-01-28 18:23:39 -0500

    added comment for database name re: debug

--- server/Config/connection.js
+++ server/Config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-//requires new database name for localhost/
+//requires new database name for localhost/  where am i determining mongo database name?
 
 // mongoose.connect(
 //   process.env.MONGODB_URI || 'mongodb://localhost/the-daily-rebound',
