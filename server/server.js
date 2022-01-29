23cdfe29104812f613b997d5d1b0d8c8c2ba4646
GitPit <rifflifter@gmail.com>
2022-01-29 16:19:59 -0500

    Config/connection

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { authMiddleware } = require('./Utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+const db = require('./Config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
