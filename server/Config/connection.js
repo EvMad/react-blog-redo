7cfeb72a15e5ba63d82a4f9ea1205daf54bd3e50
GitPit <rifflifter@gmail.com>
2022-01-31 16:40:41 -0500

    succesful npm run start / local host graphql

--- server/Config/connection.js
+++ server/Config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 //new database created through MongoDB 1/31/22 and added to Heroku config vars
 
-//npm run start error from here:
+//npm run start error from here, corrected re Mongo updates. npm run start worked on 1/31/22.
 
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/react-blog-redo',
