d6d2db4ee64f0e1c407b940558e1050ef0571cd8
GitPit <rifflifter@gmail.com>
2022-02-01 18:45:17 -0500

    browser error regarding build path needs debug

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'build')));
 }
 
-
+//getting browser error for fill path server/build/index.html
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
