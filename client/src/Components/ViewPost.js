56c0c4576781124499e3f80e00dba3be1c187815
GitPit <rifflifter@gmail.com>
2022-02-02 20:01:40 -0500

    import './css/viewpost.css';

--- client/src/Components/ViewPost.js
+++ client/src/Components/ViewPost.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Redirect, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
+import './css/viewpost.css';
 
 
 const ViewPost = () => {
