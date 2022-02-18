59c40b74922ef4db2781ff6f7e2b9d2ca1fc3ce0
GitPit <rifflifter@gmail.com>
2022-02-18 17:18:55 -0500

    import { QUERY_POST } from Utils/queries re debug

--- client/src/Components/ViewPost.js
+++ client/src/Components/ViewPost.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Redirect, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
+import { QUERY_POST } from '../Utils/queries';
 import './css/viewpost.css';
 
 
