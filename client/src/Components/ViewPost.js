f7493fdf71946be4d75395df2d87c93377eba057
GitPit <rifflifter@gmail.com>
2022-03-10 22:57:58 -0500

    changed post to Post for npm run build debug

--- client/src/Components/ViewPost.js
+++ client/src/Components/ViewPost.js
@@ -28,8 +28,8 @@ const ViewPost = () => {
                 <div className="image">
                     <img></img>
                 </div>
-                <h2 className="postHeading">{post.postTitle}</h2>
-                <p className="postContent">{post.postContent}</p>
+                <h2 className="postHeading">{Post.postTitle}</h2>
+                <p className="postContent">{Post.postContent}</p>
             </div>
 
         </div>
