d98de2abdfce4fa6c11fe543f29611d6ac665f50
GitPit <rifflifter@gmail.com>
2022-07-07 13:38:30 -0400

    'Routes' is not exported from 'react-router-dom'.

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import {
   createHttpLink,
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './App.css';
 
 
